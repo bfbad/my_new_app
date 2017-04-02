6e148e665f52446b6cb57af46ce8ff362ef3a37f
Brandon Ferguson <brfergus2@gmail.com>
2017-04-02 12:41:40 -0400

    4.2 Achievement DB updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!",
+	:colour => "Tan", :price => 1400.10, 
+	:image_url: "https://brandonsbikes.herokuapp.com/assets/racebike-0a8f0f5fb70fe9c8961c372cc3271303459239c92fa9a8c6b2c5f370513737b8.png")

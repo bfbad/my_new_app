9e938e545f4a8ee90bd5b280c23513c5ca745e73
Brandon Ferguson <brfergus2@gmail.com>
2017-04-11 23:10:48 -0400

    Update Site Root and added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/index'
-  root 'static_pages#landing_page'
+  get 'static_pages/landing_page'
+  root 'products#index'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

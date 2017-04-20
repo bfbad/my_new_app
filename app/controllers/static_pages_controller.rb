3ed7638481040f595348c28498db86d628727db7
Brandon Ferguson <brfergus2@gmail.com>
2017-04-20 15:48:10 -0400

    5.3 contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,18 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  #manage contact form
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'brfergus2@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
+
   def landing_page
   	@products = Product.limit(3)
   end

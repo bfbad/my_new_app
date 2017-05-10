5ba7459c500c1dded6178e7709f7e432f9b1c87e
Brandon Ferguson <brfergus2@gmail.com>
2017-05-10 19:55:35 -0400

    5.9 pagination for comments and validation of product and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,13 @@
 class Comment < ApplicationRecord
+  validates :user, presence: true
+  validates :body, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
   belongs_to :user
   belongs_to :product
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
   scope :five_star_comments, -> { where("rating == 5")}
   scope :one_star_comments, -> { where("rating ==1")}
+  self.per_page=5
 end

55f335b5b1c8e92337d50c8ab0c2a886dbe62937
Brandon Ferguson <brfergus2@gmail.com>
2017-05-23 07:55:02 -0400

    6.3 functional tests

--- Gemfile
+++ Gemfile
@@ -40,11 +40,13 @@ gem 'jbuilder', '~> 2.5'
 ## Gemfile for Rails 3+, Sinatra, and Merb
 gem 'will_paginate', '~> 3.1.1'
 
+# Rails controlling testing gem for functional tests.
+gem 'rails-controller-testing'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.5'
-  
 end
 
 group :development do
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,7 @@ describe Product do
   context "when the product has comments" do
     before do
       @product = Product.create!(name: "race bike")
-      @user = User.create!(email: "b@c.com", password: "123456")
+      @user = User.create!(email: "b@c.com", password: "123456", confirmed_at: Time.now)
       @product.comments.create!(rating: 1, user: @user, body: "awful bike")
       @product.comments.create!(rating: 3, user: @user, body: "okay bike")
       @product.comments.create!(rating: 5, user: @user, body: "great bike")

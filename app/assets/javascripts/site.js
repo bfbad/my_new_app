54af6cd3d75d77e6ba4c67c78599ca9315305ef7
Brandon Ferguson <brfergus2@gmail.com>
2017-06-10 16:42:06 -0400

    6.10 ajax calls for product reviews

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,14 @@
-$(document).on('turbolinks:load', function(){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+$(document).on('turbolinks:load ajaxSuccess', function(){
+  refreshRating();
+  $('.img-zoom').elevateZoom({zoomWindowPosition: 1, zoomWindowOffetx: 10});
+});
+
+var refreshRating = function() {
+$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
   $('.rated').raty( { path: '/assets',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
     }
   });
-  $('.img-zoom').elevateZoom({zoomWindowPosition: 1, zoomWindowOffetx: 10});
-});
+}

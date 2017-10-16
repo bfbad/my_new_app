692a7ab714826d11b81dc067e5ff4d015cf1be84
Brandon Ferguson <brfergus2@gmail.com>
2017-10-16 10:51:21 -0400

    update comments per page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,12 +7,12 @@ class Comment < ApplicationRecord
 
   belongs_to :user
   belongs_to :product
-  
+
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
   scope :five_star_comments, -> { where("rating == 5")}
   scope :one_star_comments, -> { where("rating ==1")}
-  
+
   # will_paginate comments per page
   def self.per_page
     5
